0e080aae6e722c2cb269c96c6387a754649eb269
Vinny <vinnywalker96@icloud.com>
2022-11-16 13:19:02 -0800

    simple_shell

---
+++ exec.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+  * main - execve
+  *
+  * Return: 0 Always
+  */
+
+int main(void)
+{
+	char *argv[] = {"/bin/ls", "-l", "/usr/", NULL};
+
+	printf("Before execve\n");
+	if (execve(argv[0], argv, NULL) == -1)
+	{
+		perror("Error:");
+	}
+	printf("After execve\n");
+	return (0);
+}
